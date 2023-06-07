df1447ed7fe9ea5b2d1690a197dd31b3e321ca67
TangiresH <dimon4ik1010@gmail.com>
2023-06-07 20:14:42 +0300

    сreated a matrix using Alpine as a foundation

--- Python/spaceship/routers/api.py
+++ Python/spaceship/routers/api.py
@@ -1,8 +1,20 @@
 from fastapi import APIRouter
+import numpy as nmp
+
 
 router = APIRouter()
 
 
-@router.get('')
-def hello_world() -> dict:
-    return {'msg': 'Hello, it is a tangiresH. How are you?'}
+@router.get('/mtrx')
+def matrix() -> dict:
+    matrix_a = nmp.random.rand(10, 10)
+    matrix_b = nmp.random.rand(10, 10)
+    product = nmp.dot(matrix_a, matrix_b)
+
+    result = {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist()
+    }
+
+    return result
